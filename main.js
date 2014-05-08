0a0ea3461123706e9f3377c1ff145e370ccb6247
Yasunori Mahata <mahata777@gmail.com>
2014-05-07 22:23:13 -0700

    Adding context-menu which does XMLHttpRequest - WIP

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 function onClickHandler(info, tab) {
-    console.log(info);
-    console.log(tab);
+    console.log(info, tab);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://github.com/mahata", true);
+    xhr.responseType = "document";
+    xhr.onload = function(e) {
+	if (this.status === 200) {
+	    console.log(this.responseXML.title);
+	}
+    };
+    xhr.send();
 };
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.contextMenus.create({
-    "title": "title",
-    "id": "dummy id"
-  });
+    // normal context menu
+    chrome.contextMenus.create({
+	"title": "title1",
+	"id": "id1"
+    });
+
+    // context menu for text selection and image
+    chrome.contextMenus.create({
+	"title": "title2",
+	"contexts": ["selection", "image"],
+	"id": "id2"
+    });    
 });
